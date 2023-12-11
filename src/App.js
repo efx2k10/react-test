b24c2f68d04c9051d831108e0384ebaffd0eb518
efx2k10 <efx2k10@google.com>
2023-12-11 12:43:50 +0300

    hw 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { restaurants } from "./constants/fixtures"
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        restaurants?.map((restaurant) => (
+          <div key={restaurant.id}>
+
+            <h2>{restaurant.name}</h2>
+
+            {restaurant.menu.length > 0 && (
+              <ul>
+                {restaurant.menu.map((dish) => (
+                  <li key={dish.id}>{dish.name}</li>
+                ))}
+              </ul>
+
+            )}
+
+            {restaurant.reviews.length > 0 && (
+              <ul>
+                {restaurant.reviews.map((review) => (
+                  <li key={review.id}>{review.user} : {review.text}</li>
+                ))}
+              </ul>
+
+            )}
+
+          </div>
+        ))
+      }
+
     </div>
   );
 }
